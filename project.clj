f259a593c9506822acc26b54a6338589e0bf33da
Silas Davis <silas@swiftkey.net>
2014-05-10 23:41:50 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hcpc-scraper "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.16"]
+                 [me.raynes/laser "1.1.1"]])
---
+++ test/hcpc_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns hcpc-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [hcpc-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
