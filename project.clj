e34d72a68e711b7930a734f266734dc04b7c6760
Silas Davis <silas@swiftkey.net>
2014-05-11 00:55:43 +0100

    added csv writing

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.csv "0.1.2"]
                  [http-kit "2.1.16"]
                  [me.raynes/laser "1.1.1"]])
--- src/hcpc_scraper/core.clj
+++ src/hcpc_scraper/core.clj
@@ -1,1 +1,39 @@
-(ns)
+(ns hcpc-scraper.core
+  (require [org.httpkit.client :as http]
+           [me.raynes.laser :as l]
+           [clojure.data.csv :as csv]
+           [clojure.java.io :as io]
+           [clojure.pprint :refer [cl-format]]))
+
+(def hcpc-url "http://www.hpc-uk.org/search-results/search-details/")
+
+(defn pull-table [html]
+  (l/select (l/parse html) (l/descendant-of (l/and (l/element= :table) (l/class= "resultsTable"))
+                                            (l/or (l/element= :th) (l/element= :td)))))
+
+(defn person-table [id]
+  (http/get hcpc-url
+            {:query-params {:ID id :profession "PYL"}}
+            (fn [{:keys [body]}] (pull-table body))))
+
+(defn person-record [table]
+  (apply hash-map (mapcat :content table)))
+
+(defn person-tables [l-id h-id]
+  (doall (map person-table (range l-id h-id))))
+
+(defn jitter [l-id h-id]
+  (let [ms (+ 500 (rand 500))]
+    (future (do (println (cl-format nil "Jittering for ~s ms before pulling IDs ~s to ~s" ms l-id h-id))
+                (Thread/sleep ms)))))
+
+; Add a random wait before each chunk is requested to not seem like DOS attacker
+(defn person-records-chunk [l-id h-id chunk]
+  (if-let [prs (seq (map person-record (filter seq (map deref (cons (jitter l-id h-id) (person-tables l-id h-id))))))]
+    (lazy-cat prs (person-records-chunk (+ h-id 1) (+ h-id chunk) chunk))))
+
+(defn write-csv [prs filename]
+  (let [header (sort (keys (first prs)))]
+    (with-open [out-file (io/writer filename)]
+      (csv/write-csv out-file (cons header (map (fn [pr] (map pr header)) prs))))))
+
